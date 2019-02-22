c5a416aba227fb0122ca649d1c9c52a3c0d2730b
bigme666 <francesco.cavallo@gmail.com>
2019-02-22 11:12:15 +0100

    new write

---
+++ video.jl
@@ -0,0 +1,27 @@
+module Video
+
+using Images
+
+    function writevideo(fname, imgstack::Array{<:Color,3};
+                    overwrite=true, fps=30::UInt, options=``)
+        ow = overwrite ? `-y` : `-n`
+        h, w, nframes = size(imgstack)
+
+        open(`ffmpeg
+                -loglevel warning
+                $ow
+                -f rawvideo
+                -pix_fmt rgb24
+                -s:v $(h)x$(w)
+                -r $fps
+                -i pipe:0
+                $options
+                -vf "transpose=0"
+                -pix_fmt yuv420p
+                $fname`, "w") do out
+                    for i = 1:nframes
+                        write(out, convert.(RGB{N0f8}, clamp01.(imgstack[:,:,i])))
+                    end
+                end
+    end
+end
